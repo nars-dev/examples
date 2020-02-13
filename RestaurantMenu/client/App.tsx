1811b94a2a0616804cb5d3f2b7a7f09a0873bdcb
Wojtek Czekalski <me@wczekalski.com>
2020-02-13 12:22:03 +0100

    Remove separate Menu file on the client

--- RestaurantMenu/client/App.tsx
+++ RestaurantMenu/client/App.tsx
@@ -8,17 +8,49 @@
  * @format
  */
 
-import React from 'react';
-import {StatusBar} from 'react-native';
-import {Menu} from './Menu';
+import * as React from 'react';
+import {
+  Alert,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  StatusBar,
+} from 'react-native';
+import {config} from 'RestaurantMenu-config';
+import * as Nars from 'nars-client';
 
 declare var global: {HermesInternal: null | {}};
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#EEE',
+  },
+  header: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+const {Menu} = Nars.createRemoteComponentWithUrl('ws://localhost:9000', config);
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <Menu />
+      <SafeAreaView>
+        <View style={styles.headerContainer}>
+          <Text style={styles.header}>{'Menu'}</Text>
+        </View>
+        <Menu
+          onMenuItemSelected={(_, title) => {
+            Alert.alert(`Tapped "${title}"`);
+          }}
+        />
+      </SafeAreaView>
     </>
   );
 };
--- RestaurantMenu/config/src/Config.ts
+++ RestaurantMenu/config/src/Config.ts
@@ -4,6 +4,7 @@ import { InputProp, localCallback } from "nars-common";
  * Define Routes and the props
  */
 const title = InputProp.string;
+
 export const config = {
   Menu: {
     onMenuItemSelected: localCallback(InputProp.void, title)
